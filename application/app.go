01d932a796ddd472b969e06d1b8d637e062f106f
Alexandros Collie <amcollie@gmail.com>
2023-11-06 01:55:58 -0500

    Graceful shutdown complete

--- application/app.go
+++ application/app.go
@@ -5,17 +5,21 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/handlers"
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
@@ -28,10 +32,37 @@ func (app *App) Start(ctx context.Context) error {
 		Handler: loggedRouter,
 	}
 
-	err := server.ListenAndServe()
+	err := app.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to listen to server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
-	return nil
+	defer func() {
+		if err := app.rdb.Close(); err != nil {
+			fmt.Printf("failed to close redis: %v\n", err)
+		}
+	}()
+
+	fmt.Println("Starting server on port 3000...")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to listen to server: %w", err)
+		}
+
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
 }
